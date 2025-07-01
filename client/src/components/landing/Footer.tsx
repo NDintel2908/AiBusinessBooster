7c30aa8cf90f31b5422f4c9d3985fdda362af5dc
ND_Intel <78247310+NDintel2908@users.noreply.github.com>
2025-07-01 10:13:32 +0000

    sua dia chi

--- client/src/components/landing/Footer.tsx
+++ client/src/components/landing/Footer.tsx
@@ -118,7 +118,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                Địa chỉ: 224A Điện Biên Phủ, Phường Võ Thị Sáu, Quận 3, TPHCM
+                Địa chỉ: 224A Điện Biên Phủ, Phường Xuân Hòa, TPHCM
               </li>
               <li className="flex items-center gap-2 text-gray-400">
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
